d085e9c6f8b1ae0798cebf33b95ef9d77139d7a7
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2016-01-14 09:51:15 -0200

    creating one page per prize

--- app/src/main/java/com/karina/alicesadventures/PrizesActivity.java
+++ app/src/main/java/com/karina/alicesadventures/PrizesActivity.java
@@ -198,20 +198,8 @@ public class PrizesActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return items.size();
         }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
     }
 }
